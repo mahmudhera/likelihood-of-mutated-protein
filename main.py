b80f5ebb96b761da8bbe9cb467011b61d95b1693
Mahmudur <mahmudhera93@gmail.com>
2022-08-29 20:37:59 -0400

    added code to compute probability of a syn/nonsyn mutation given a codon

--- main.py
+++ main.py
@@ -38,8 +38,30 @@ def determine_likelihood_of_mutated_protein(p):
                 total_likelihood += (1 - p) ** num_same * (p/3) ** num_diff * (1.0 / 64)
     return total_likelihood
 
+def determine_likelihood_of_mutated_protein_given_codon(codon, p):
+    # given: codon: 3-mer string
+    # p: mutation rate
+    l = list(product('ACGT', repeat=3))
+    likelihood_nonsyn = 0.0
+    likelihood_syn = 0.0
+    for mutated in l:
+        mut_codon = "".join(mutated)
+
+        num_same = 0
+        if codon[0] == mut_codon[0]: num_same += 1
+        if codon[1] == mut_codon[1]: num_same += 1
+        if codon[2] == mut_codon[2]: num_same += 1
+        num_diff = 3 - num_same
+
+        if protein_dic[codon] != protein_dic[mut_codon]:
+            likelihood_nonsyn += (1 - p) ** num_same * (p/3) ** num_diff
+        elif codon != mut_codon:
+            likelihood_syn += (1 - p) ** num_same * (p/3) ** num_diff
+
+    return likelihood_syn, likelihood_nonsyn
 
 if __name__ == "__main__":
+    """
     ps = [1.0 * i / 100 for i in range(101)]
     ls = [determine_likelihood_of_mutated_protein(p) for p in ps]
     plt.plot(ps, ls)
@@ -47,3 +69,10 @@ if __name__ == "__main__":
     plt.xlabel('Nucleotide mutation rate (given)')
     plt.ylabel('AA mutation rate in a codon (computed)')
     plt.savefig('p_aa_vs_p_nt.pdf')
+    """
+
+    p = 0.03
+    all_codons = [ "".join(x) for x in list(product('ACGT', repeat=3)) ]
+    for codon in all_codons:
+        syn_mut_prob, nonsyn_mut_prob = determine_likelihood_of_mutated_protein_given_codon(codon, p)
+        print(codon, syn_mut_prob, nonsyn_mut_prob, syn_mut_prob+nonsyn_mut_prob, 1.0-(1.0-p)**3)
